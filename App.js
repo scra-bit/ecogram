958aa39387b1412eb62e8fa4e4d98b7cc05915f1
scra-bit <143202652+scra-bit@users.noreply.github.com>
2024-07-21 13:34:42 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const { width } = Dimensions.get("window");
+
+export default class App extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      this.scrollView.scrollTo({ y: -30 });
+    }, 1);
+  }
+
+  render() {
+    return (
+      <ScrollView
+        ref={(scrollView) => {
+          this.scrollView = scrollView;
+        }}
+        style={styles.container}
+        pagingEnabled
+        decelerationRate="fast"
+        snapToInterval={220}
+        snapToAlignment="center"
+        contentContainerStyle={styles.contentContainer}
+      >
+        {[...Array(10)].map((_, index) => (
+          <View key={index} style={styles.card}>
+            <postview />
+          </View>
+        ))}
+      </ScrollView>
+    );
+  }
 }
+
+const postview = () => (
+  <View>
+    <Image
+      source={{ uri: "https://placekitten.com/200/300" }}
+      style={styles.image}
+    />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  contentContainer: {
+    paddingVertical: 40,
+  },
+  card: {
+    width: width - 30,
+    marginVertical: 10,
+    height: innerHeight,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+  },
+
+  image: {
+    borderRadius: 18,
+    width: innerWidth,
+    height: innerHeight,
+    marginTop: 10,
   },
 });
